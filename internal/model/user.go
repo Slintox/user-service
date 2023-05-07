3fb87f3bdc48680599db3ba833523b494ab488f5
a.saharov <a.saharov@ficto.ru>
2023-05-07 19:41:35 +0300

    G1: Ref: removed comments

--- internal/model/user.go
+++ internal/model/user.go
@@ -8,27 +8,27 @@ type UserRole int
 
 // User описывает модель пользователя
 type User struct {
-	Username  string    // Unique
-	Email     string    `json:"email"`
-	Password  string    `json:"password"`
-	Role      UserRole  `json:"role"`
-	CreatedAt time.Time `json:"createdAt"`
-	UpdatedAt time.Time `json:"updatedAt"`
+	Username  string // Unique
+	Email     string
+	Password  string
+	Role      UserRole
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
 
 // CreateUser описывает модель для создания нового пользователя
 type CreateUser struct {
-	Username        string   `json:"username"` // Unique
-	Email           string   `json:"email"`
-	Password        string   `json:"password"`
-	ConfirmPassword string   `json:"confirmPassword"`
-	Role            UserRole `json:"role"`
+	Username        string // Unique
+	Email           string
+	Password        string
+	ConfirmPassword string
+	Role            UserRole
 }
 
 // UpdateUser описывает модель для обновления пользователя
 type UpdateUser struct {
-	Username *string   `json:"username"`
-	Email    *string   `json:"email"`
-	Password *string   `json:"password"`
-	Role     *UserRole `json:"role"`
+	Username *string
+	Email    *string
+	Password *string
+	Role     *UserRole
 }
